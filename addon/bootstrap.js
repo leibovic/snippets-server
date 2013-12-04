3d947645e3f6eaaa189de3669eae187e482c8289
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-12-04 14:42:58 -0800

    create add-on to flip snippets prefs to use test snippets server

---
+++ addon/bootstrap.js
@@ -0,0 +1,76 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+function loadIntoWindow(window) {
+  Services.prefs.setBoolPref("browser.snippets.enabled", true);
+  Services.prefs.setCharPref("browser.snippets.updateUrl", "http://snippets-server.paas.allizom.org/");
+}
+
+function unloadFromWindow(window) {
+  Services.prefs.clearUserPref("browser.snippets.enabled");
+  Services.prefs.clearUserPref("browser.snippets.updateUrl");
+}
+
+/**
+ * bootstrap.js API
+ */
+var windowListener = {
+  onOpenWindow: function(aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+  
+  onCloseWindow: function(aWindow) {
+  },
+  
+  onWindowTitleChange: function(aWindow, aTitle) {
+  }
+};
+
+function startup(aData, aReason) {
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+  // Load into any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    loadIntoWindow(domWindow);
+  }
+
+  // Load into any new windows
+  wm.addListener(windowListener);
+}
+
+function shutdown(aData, aReason) {
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN)
+    return;
+
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+  // Stop listening for new windows
+  wm.removeListener(windowListener);
+
+  // Unload from any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
